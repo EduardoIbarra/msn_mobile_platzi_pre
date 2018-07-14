751d07a2775768b6948c5b5a58f5cab058943b6d
Eduardo de la Cruz Ibarra <eduardoibarra904@gmail.com>
2018-07-14 14:26:29 -0500

    Integrated angularfire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,22 @@ import { ListPage } from '../pages/list/list';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import {LoginPage} from "../pages/login/login";
-import {ConversationPage} from "../pages/conversation/conversation";
-import {UserService} from "../services/user";
-import {SearchPipe} from "../pipes/search";
-import {AngularFontAwesomeModule} from "angular-font-awesome";
-
+import {LoginPage} from '../pages/login/login';
+import {ConversationPage} from '../pages/conversation/conversation';
+import {UserService} from '../services/user';
+import {SearchPipe} from '../pipes/search';
+import {AngularFontAwesomeModule} from 'angular-font-awesome';
+import {AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import {AngularFireAuthModule} from 'angularfire2/auth';
+export const firebaseConfig = {
+  apiKey: 'AIzaSyB1WZRwZRHy9L6jwgqYgOlTvf7yPCC33hQ',
+  authDomain: 'msnalive2.firebaseapp.com',
+  databaseURL: 'https://msnalive2.firebaseio.com',
+  projectId: 'msnalive2',
+  storageBucket: 'msnalive2.appspot.com',
+  messagingSenderId: '18390215590'
+};
 @NgModule({
   declarations: [
     MyApp,
@@ -26,7 +36,10 @@ import {AngularFontAwesomeModule} from "angular-font-awesome";
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
