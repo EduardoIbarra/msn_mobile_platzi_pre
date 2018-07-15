6dc9cf1e48fa845a5c54fda03332875b92f2472d
Eduardo de la Cruz Ibarra <eduardoibarra904@gmail.com>
2018-07-15 17:16:25 -0500

    accepting friends

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import {AlertController, ModalController, Nav, Platform, ToastController} from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
 import {ProfilePage} from "../pages/profile/profile";
+import {User} from "../interfaces/user";
+import {RequestService} from "../services/request";
+import {UserService} from "../services/user";
+import {AuthService} from "../services/auth";
 
 @Component({
   templateUrl: 'app.html'
@@ -15,8 +19,16 @@ export class MyApp {
   rootPage: any = HomePage;
 
   pages: Array<{title: string, component: any}>;
-
-  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
+  user: any;
+  requests: any;
+  mailsShown: any = [];
+  constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen, private usersService: UserService,
+              private requestService: RequestService,
+              private userService: UserService,
+              private modalCtrl: ModalController,
+              private toastCtrl: ToastController,
+              public alertCtrl: AlertController,
+              private authService: AuthService) {
     this.initializeApp();
 
     // used for an example of ngFor and navigation
@@ -25,6 +37,12 @@ export class MyApp {
       { title: 'Perfil', component: ProfilePage }
     ];
 
+    this.authService.getStatus().subscribe((result) => {
+      this.userService.getById(result.uid).valueChanges().subscribe((user: User) => {
+        this.user = user;
+        this.getFriendRequests();
+      });
+    });
   }
 
   initializeApp() {
@@ -41,4 +59,57 @@ export class MyApp {
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
+  getFriendRequests() {
+    this.requestService.getRequestsForEmail(this.user.email).valueChanges().subscribe( (requests: any) => {
+      this.requests = requests.filter((r) => {
+        return r.status !== 'accepted' && r.status !== 'rejected';
+      });
+      this.requests.forEach((r) => {
+        if(this.mailsShown.indexOf(r.sender.email) === -1) {
+          this.mailsShown.push(r.sender.email);
+          this.showRadio(r);
+        }
+      });
+    });
+  }
+
+  showRadio(r) {
+    let alert = this.alertCtrl.create();
+    alert.setTitle('¡Solicitud de Amistad!');
+    alert.setMessage(r.sender.email + 'te ha invitado a ser su amigo, deseas aceptar?');
+
+    alert.addInput({
+      type: 'radio',
+      label: 'Sí.',
+      value: 'yes',
+      checked: true
+    });
+    alert.addInput({
+      type: 'radio',
+      label: 'No, gracias.',
+      value: 'no',
+      checked: false
+    });
+
+    alert.addButton({
+      text: 'OK',
+      handler: data => {
+        if (data === 'yes') {
+          this.requestService.setRequestStatus(r, 'accepted').then(() => {
+            this.userService.addFriend(this.user.uid, r.sender.uid);
+          });
+        } else {
+          this.requestService.setRequestStatus(r, 'rejected').then(() => {});
+        }
+      }
+    });
+    alert.addButton({
+      text: 'Decidir más Tarde',
+      handler: data => {
+        this.requestService.setRequestStatus(r, 'decide_later').then(() => {});
+      }
+    });
+    return alert.present();
+  }
 }
+
--- src/services/request.ts
+++ src/services/request.ts
@@ -9,4 +9,12 @@ export class RequestService {
     const cleanEmail = request.receiver_email.replace('.', ',');
     return this.angularFireDataBase.object('requests/' + cleanEmail + '/' + request.sender.uid).set(request);
   }
+  setRequestStatus(request, status){
+    const cleanEmail = request.receiver_email.replace('.', ',');
+    return this.angularFireDataBase.object('requests/' + cleanEmail + '/' + request.sender.uid + '/status').set(status);
+  }
+  getRequestsForEmail(email) {
+    const cleanEmail = email.replace('.', ',');
+    return this.angularFireDataBase.list('requests/' + cleanEmail);
+  }
 }
